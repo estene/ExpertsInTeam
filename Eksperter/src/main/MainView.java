662fc0634a9b6b9fc47b6232376682d429f9d35a
Even Stene <najtrox@gmail.com>
2012-03-21 13:12:46 +0100

    Added resources

--- Eksperter/src/main/MainView.java
+++ Eksperter/src/main/MainView.java
@@ -3,10 +3,19 @@ package main;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JList;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.ButtonGroup;
+import javax.swing.ImageIcon;
 
 public class MainView {
 
-	private JFrame frame;
+	private JFrame mainFrame;
+	private final ButtonGroup buttonGroup = new ButtonGroup();
 
 	/**
 	 * Launch the application.
@@ -16,7 +25,7 @@ public class MainView {
 			public void run() {
 				try {
 					MainView window = new MainView();
-					window.frame.setVisible(true);
+					window.mainFrame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -35,9 +44,38 @@ public class MainView {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		mainFrame = new JFrame();
+		mainFrame.setBounds(100, 100, 720, 445);
+		mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		mainFrame.getContentPane().setLayout(null);
+		
+		JPanel panel = new JPanel();
+		panel.setBounds(10, 11, 684, 385);
+		mainFrame.getContentPane().add(panel);
+		panel.setLayout(null);
+		
+		JLabel animationLabel = new JLabel("");
+		animationLabel.setIcon(new ImageIcon(MainView.class.getResource("/prinsenkryssetmed.png")));
+		animationLabel.setBounds(305, 5, 369, 333);
+		panel.add(animationLabel);
+		
+		JList messageList = new JList();
+		messageList.setBounds(10, 5, 285, 277);
+		panel.add(messageList);
+		
+		JButton btnNewButton = new JButton("");
+		btnNewButton.setIcon(new ImageIcon(MainView.class.getResource("/play2.png")));
+		buttonGroup.add(btnNewButton);
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		btnNewButton.setBounds(20, 293, 113, 81);
+		panel.add(btnNewButton);
+		
+		JButton button = new JButton("");
+		button.setSelectedIcon(new ImageIcon(MainView.class.getResource("/stop2.png")));
+		button.setBounds(143, 293, 113, 81);
+		panel.add(button);
 	}
-
 }
